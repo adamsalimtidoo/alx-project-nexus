8f84815d9b200034a7471581453d0ae8054dcf66
adamsalimtidoo <adamsalimtidoo@gmail.com>
2025-09-28 02:24:01 +0000

    Updated setup

--- airbnb-clone/src/App.tsx
+++ airbnb-clone/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div className='w-[100vw] h-[100vh] bg-primary-100'></div>
+   <div className='w-[100vw] h-[100vh] bg-primary-100'>
+    <h1 className="font-poppins text-4xl">Hello World</h1>
+    <h1 className="font-open_sans text-4xl">Hello World</h1>
+   </div>
   )
 }
 
--- airbnb-clone/tailwind.config.ts
+++ airbnb-clone/tailwind.config.ts
@@ -6,10 +6,20 @@ export default {
     extend: {
       colors: {
         primary: {
-          "100": "#b0b0b0",
+          "100": "#FF385C",
+          "200": "#FC642D"
         },
+        accent:"#00A699",
+        grey: {
+          "100": "#717171",
+          "200": "#EBEBEB",
+          "300": "#DDDDDD",
+        }
       },
-      fontFamily: {},
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        open_sans: ["Open San", "sans-serif"]
+      },
     },
   },
   plugins: [],
